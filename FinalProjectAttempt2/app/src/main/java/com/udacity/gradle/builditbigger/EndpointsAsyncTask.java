870707ba44515899463461e39daed270ed1d7d12
Gabriella Hawkes <appsalothelp@gmail.com>
2020-06-13 13:55:41 -0500

    Correct product flavor files

--- FinalProjectAttempt2/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ FinalProjectAttempt2/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -23,7 +23,6 @@ class EndpointsAsyncTask extends AsyncTask<EndpointsAsyncTask.AfterExecution, Vo
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
                     .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                    .setApplicationName("Build it Bigger")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
@@ -53,4 +52,6 @@ class EndpointsAsyncTask extends AsyncTask<EndpointsAsyncTask.AfterExecution, Vo
     public interface AfterExecution{
         void afterExecute(String string);
     }
+
+
 }
